3a442c1884f5f5944718248d891578e0bf2ba682
Student User <studentuser@sheltonstate.edu>
2025-01-09 15:16:20 -0600

    Shunting Yard Algorithm Complete?

--- modules/handle_input.js
+++ modules/handle_input.js
@@ -33,6 +33,7 @@ export function handle_input(input_str){
         let tokens = tokenize(input_str);
         let output = [];
         let operator_stack = [];
+        console.log("Regular Tokens")
         tokens.forEach(function(token, index) {  
             console.log(index + "=> " + token.type + "(" + token.value + ")");
             if(token.type == "Literal"){
@@ -43,15 +44,85 @@ export function handle_input(input_str){
             }
             else if(token.type == "Operator"){
                 while(true){
+                    //console.log("while 1")
                     if(operator_stack.length > 0){
                         if(operator_stack.at(-1).value != "("){
-                            
+                            let p1 = [0, "right"]
+                            let p2 = [0, "right"]
+                            operator_table.forEach(function(symbol, precedence, associativity){
+                                if(symbol == operator_stack.at(-1).value){
+                                    p2 = [precedence, associativity]
+                                }
+                                if(symbol == token.value){
+                                    p1 = [precedence, associativity]
+                                }
+                            });
+                            if(p2.at(0) > p1.at(0)){
+                                output.push(operator_stack.pop())
+                            }
+                            else if(p2.at(0) == p1.at(0)){
+                                if(p1.at(1) === "left"){
+                                    output.push(operator_stack.pop())
+                                }
+                                break
+                            }
+                            else{
+                                break
+                            }
+                        }
+                        else{
+                            break
                         }
                     }
+                    else{
+                        break
+                    }
+                }
+                operator_stack.push(token)
+            }
+            else if(token.type == "Function Argument Separator"){
+                while(operator_stack.at(-1).value != "("){
+                    //console.log("while 2")
+                    output.push(operator_stack.pop())
                 }
             }
-
-
+            else if(token.type == "Left Paranthesis"){
+                operator_stack.push(token)
+            }
+            else if(token.type == "Right Paranthesis"){
+                while(true){
+                    //console.log("while 3")
+                    if(operator_stack.length == 0){
+                        console.log("Mismatched Parantheses error")
+                        break
+                    }
+                    else if(operator_stack.at(-1).type == "Left Paranthesis"){
+                        break
+                    }
+                    else{
+                        output.push(operator_stack.pop())
+                    }
+                }
+                if(operator_stack.at(-1).type != "Left Paranthesis"){
+                    console.log("Mismatched Parantheses error")
+                }
+                else{
+                    operator_stack.pop()
+                }
+            }
+        });
+        while(true){
+            //console.log("while 4")
+            if(operator_stack.length == 0){
+                break
+            }
+            else{
+                output.push(operator_stack.pop())
+            }
+        }
+        console.log("RPN tokens")
+        output.forEach(function(token, index) {  
+            console.log(index + "=> " + token.type + "(" + token.value + ")");
         });
         return tokens;
     }
