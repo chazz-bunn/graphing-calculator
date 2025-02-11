492eedd0e2f99db552d3dffd646e5ce47960d75f
Student User <studentuser@sheltonstate.edu>
2025-02-11 17:05:13 -0600

    not much

--- modules/input_box.js
+++ modules/input_box.js
@@ -1,24 +1,38 @@
 export class InputBox{
-    constructor(idx, width, height, box_active = false){
+    constructor(idx, width, height){
         this.input_canvas = document.createElement('canvas');
         this.input_canvas.tabIndex = 0;
         this.input_canvas.id = "input-canvas-" + idx.toString();
         this.input_canvas.className = "input-canvas";
         this.ctx = this.input_canvas.getContext('2d');
-        this.font_size = 22;
+
         this.ctx.canvas.height = height;
         this.ctx.canvas.width = width;
-        this.box_active = box_active;
+        
         this.cursorBlickInterval = 500;
         this.isCursorVisible = true;
         this.is_focused = false;
+
+        this.font_size = 22;
         this.input_str = "";
+        this.cursor_index = 0;
 
         this.input_canvas.addEventListener('keydown', (event)=>{
             if(event.key.length == 1){
                 //console.log(event);
                 this.input_str += event.key;
+                this.cursor_index++;
                 this.drawCursor();
+            }
+            else if(event.key == "ArrowLeft"){
+                if(this.cursor_index > 0){
+                    this.cursor_index--;
+                }
+            }
+            else if(event.key == "ArrowRight"){
+                if(this.cursor_index < this.input_str.length){
+                    this.cursor_index++;
+                }
             }
         });
         this.input_canvas.addEventListener("focusin", ()=>{
@@ -45,11 +59,11 @@ export class InputBox{
         this.ctx.clearRect(0, 0, this.input_canvas.width, this.input_canvas.height);
         this.ctx.font = this.font_size.toString() + "px Arial";
         this.ctx.fillStyle = "black";
+        //Redo all this
+        this.ctx.fillText(this.input_str, 4, this.ctx.canvas.height/2 + this.font_size/2 - 3);
         if(this.isCursorVisible && this.is_focused){
-            this.ctx.fillText(this.input_str + "|", 4, this.ctx.canvas.height/2 + this.font_size/2 - 3);
-        }
-        else{
-            this.ctx.fillText(this.input_str, 4, this.ctx.canvas.height/2 + this.font_size/2 - 3);
+            console.log(4 + this.font_size/2*this.cursor_index);
+            this.ctx.fillText("|", 4 + this.font_size/2*this.cursor_index, this.ctx.canvas.height/2 + this.font_size/2 - 3);
         }
     }
 
